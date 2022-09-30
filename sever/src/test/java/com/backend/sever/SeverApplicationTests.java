e8260803c2a961aa6fdcf118bf22cb09c1e6dd06
ricky <ricky.ilinke@gmail.com>
2022-09-30 23:19:46 +0800

    n

--- sever/src/test/java/com/backend/sever/SeverApplicationTests.java
+++ sever/src/test/java/com/backend/sever/SeverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.backend.sever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SeverApplicationTests {
 
 	@Test
 	void contextLoads() {
